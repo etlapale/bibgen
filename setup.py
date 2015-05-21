5211a570eaa62c4ceca41654be3dbd69d3f8109a
Émilien Tlapale <emilien@tlapale.com>
2015-05-21 11:51:27 -0700

    Updating description and license in setup.py.

--- setup.py
+++ setup.py
@@ -6,23 +6,26 @@ from setuptools import setup
 setup(name='bibgen',
       version='0.2',
       description='A citation and bibliography generator',
-      long_description='''bibgen is a Python script that allows styled
-      citations and bibliography generation using external
-      bibliographic databases. It supports databases such as BibTeX,
-      JSON or Mendeley, and generates styled citations and
-      bibliography sections for several document types including
-      DocBook. Textual citations and entries in the generated
-      bibliographies are styled using rules found in CSL
-      stylesheets. Thousands of those stylesheets are readily
-      available, for instance through the Zotero style repository.''',
+      long_description='''Bibgen is a Python script that allows styled
+citations and bibliography generation using external bibliographic
+databases. It supports databases such as BibTeX, JSON or Mendeley,
+and generates styled citations and bibliography sections for several
+document types including DocBook and reStructuredText. Textual
+citations and entries in the generated bibliographies are styled using
+rules found in CSL stylesheets. Thousands of those stylesheets are
+readily available, for instance through the Zotero style repository.''',
       author='Émilien Tlapale',
       author_email='emilien@tlapale.com',
+      maintainer='Émilien Tlapale',
+      maintainer_email='emilien@tlapale.com',
       url='http://emilien.tlapale.com/code/bibgen',
+      license='BSD',
       packages=['bibgen',
                 'bibgen.citeproc',
                 'bibgen.formatter',
                ],
       scripts=['bin/bibgen'],
+      data_files= [("", ["LICENSE"])],
       install_requires = ['citeproc-py >= 0.3.0'],
       extras_require = {
         'enhanced_bibtex':  ['bibtexparser']
