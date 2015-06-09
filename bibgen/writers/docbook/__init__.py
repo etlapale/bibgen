2af964c42d773c2528f537ecc5f1c57451e9058a
Émilien Tlapale <emilien@tlapale.com>
2015-06-08 23:41:02 -0700

    Abstract and docinfo for DocBook 5 writer.

--- bibgen/writers/docbook/__init__.py
+++ bibgen/writers/docbook/__init__.py
@@ -8,8 +8,14 @@
 
 from __future__ import absolute_import, division, print_function
 
+import docutils.nodes as nodes
+
 from .oliverr import Writer as OllieWriter
 from .oliverr import DocBookTranslator as OllieTranslator
+
+
+DOCBOOK_NS = 'http://docbook.org/ns/docbook'
+DOCBOOK_VERSION = '5.0'
 
 
 class Writer(OllieWriter):
@@ -22,6 +28,56 @@ class Writer(OllieWriter):
 class DocBookTranslator(OllieTranslator):
     def __init__(self, document):
         OllieTranslator.__init__(self, document)
+        self.doc_header = [
+            self.XML_DECL % (document.settings.output_encoding,),
+            '<%s xmlns="%s" version="%s">\n' \
+                % (self.doctype, DOCBOOK_NS, DOCBOOK_VERSION)
+        ]
+        self.doc_footer = [
+            '</%s>\n' % self.doctype
+        ]
+
+        self.ctxs = []
+
+    def push_ctx(self):
+        self.ctxs.append(self.body)
+        self.body = []
+
+    def pop_ctx(self):
+        self.body = self.ctxs.pop()
+
+    def astext(self):
+        return ''.join(self.doc_header
+                       + ['<info>\n'] + self.docinfo + ['</info>\n']
+                       + self.body
+                       + self.doc_footer)
+
+    def visit_docinfo(self, node):
+        docinfo = ['<title>%s</title>\n' % self.title]
+        for child in node:
+            if isinstance(child, nodes.date):
+                docinfo.append('<date>%s</date>\n' % child.astext())
+            elif isinstance(child, nodes.authors):
+                for a in child:
+                    docinfo.append('<author><personname>%s</personname></author>\n' % a.astext())
+            else:
+                print('unknown docinfo node: ', child)
+        self.docinfo = docinfo
+        raise nodes.SkipChildren
+
+    def visit_topic(self, node):
+        if 'abstract' in node.get('classes'):
+            self.push_ctx()
+            self.body.append(self.starttag(node, 'abstract'))
+            self.context.append('abstract')
+        else:
+            OllieTranslator.visit_topic(self, node)
+
+    def depart_topic(self, node):
+        OllieTranslator.depart_topic(self, node)
+        if 'abstract' in node.get('classes'):
+            self.docinfo.append(''.join(self.body))
+            self.pop_ctx()
         
     def visit_math(self, node):
         self.body.append('<inlineequation>\n')
