7bd8e62c8bcdf0dd46dca1c5b10b4f0982e9c52a
Émilien Tlapale <emilien@tlapale.com>
2016-04-30 12:33:15 +0200

    Support for newest citeproc-py and bibtexparser.

--- bibgen/citeproc/bibtexparser.py
+++ bibgen/citeproc/bibtexparser.py
@@ -14,44 +14,14 @@ class CiteProcBibTeX(cbt.BibTeX):
     BibTeX source for citeproc-py based on bibtexparser.
     '''
     def __init__(self, fp):
+        self.preamble_macros = {}
         parser = bibtexparser.bparser.BibTexParser()
         parser.customization = bibtexparser.customization.convert_to_unicode
         bib_db = bibtexparser.load(fp, parser=parser)
         for entry in bib_db.entries:
-            bent = cbp.BibTeXEntry(entry['type'], entry)
-            self.add(self.create_reference(entry['id'].lower(), bent))
-
-    # This functions is copy/pasted/modified from citeproc-py
-    def _bibtex_to_csl(self, bibtex_entry):
-        csl_dict = {}
-        for field, value in bibtex_entry.items():
-            try:
-                csl_field = self.fields[field]
-            except KeyError:
-                if field not in ('year', 'month', 'filename'):
-                    warnings.warn("Unsupported BibTeX field '{}'".format(field))
-                continue
-            if field in ('number', 'volume'):
-                try:
-                    value = int(value)
-                except ValueError:
-                    pass
-            elif field == 'pages':
-                if value.endswith('+'):
-                    value = citeproc.source.Pages(first=value[:-1])
-                else:
-                    fl = value.replace(' ', '').split('--')
-                    if len(fl) > 1:
-                        value = citeproc.source.Pages(first=fl[0], last=fl[1])
-                    else:
-                        value = citeproc.source.Pages(first=fl[0])
-            elif field in ('author', 'editor'):
-                value = [name for name in self._parse_author(value)]
-            else:
-                try:
-                    value = self._parse_title(value)
-                except TypeError:
-                    value = str(value)
-            csl_dict[csl_field] = value
-        return csl_dict
-
+            entry_type = entry['type'] if 'type' in entry \
+                         else entry['ENTRYTYPE']
+            entry_id = entry['id'] if 'id' in entry \
+                         else entry['ID']
+            bent = cbp.BibTeXEntry(entry_type, entry)
+            self.add(self.create_reference(entry_id.lower(), bent))
